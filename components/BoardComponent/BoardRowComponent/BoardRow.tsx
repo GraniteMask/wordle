142c8f3bd45e6b35f4fc850d5015351a095bb155
GraniteMask <ratnadeepastronomy@gmail.com>
2023-01-24 16:05:36 +0530

    added logic for backspace and arrow keys movable functionality

--- components/BoardComponent/BoardRowComponent/BoardRow.tsx
+++ components/BoardComponent/BoardRowComponent/BoardRow.tsx
@@ -24,21 +24,31 @@ export default function BoardRow({ rowIdx, onScreenInput } : Row) : JSX.Element 
     const [letterStatus, setLetterStatus] = useState<(keyof typeof stylesMap)[]>(Array(COLUMNS).fill(""))   
     const itemsRef = useRef<Array<any>>([]);
 
+    let countOfWords: any = {'F': 1, 'U': 1, 'Z': 2, 'Y': 1}
+
     useEffect(()=>{
         itemsRef.current[0].focus();      // to autofocus on first load of the page
     }, [attempts])
 
-    function compare(currentWord: Array<String>, CORRECT_WORD: Array<String>){
-        let CORRECT_WORD_TEMP = CORRECT_WORD
+    function compare(currentWord: Array<string>, CORRECT_WORD: Array<string>){
         for(let i = 0; i < currentWord.length; i++){
             let temp = letterStatus
-            if(currentWord[i] === CORRECT_WORD_TEMP[i]){
+            if(currentWord[i] === CORRECT_WORD[i]){
                 temp[i] = "green"
                 setLetterStatus(temp)
-            }else if(CORRECT_WORD_TEMP.includes(currentWord[i])){
+                countOfWords[currentWord[i]] -= 1
+                currentWord[i] = ""
+            }
+        }
+
+        for(let i = 0; i < currentWord.length; i++){
+            let temp = letterStatus
+            console.log(temp)
+            if(currentWord[i] !== "" && countOfWords[currentWord[i]] !== undefined && countOfWords[currentWord[i]] > 0){
                 temp[i] = "yellow"
                 setLetterStatus(temp)
-            }else if(!CORRECT_WORD_TEMP.includes(currentWord[i])){
+                countOfWords[currentWord[i]] -= 1
+            }else if(currentWord[i] !== "" && ((countOfWords[currentWord[i]] === undefined) || (countOfWords[currentWord[i]] === 0))){
                 temp[i] = "absent"
                 setLetterStatus(temp)
             }
@@ -56,18 +66,43 @@ export default function BoardRow({ rowIdx, onScreenInput } : Row) : JSX.Element 
         let fieldIntIndex: any = parseInt(codeFieldIndex, 10);
 
         if(fieldIntIndex < COLUMNS-1){
-            console.log(fieldIntIndex)
+            // console.log(fieldIntIndex)
+            // window.addEventListener('keypress', function(event){
+                
+            //     if(event.keyCode !== 8){
+            //         console.log("+1")
+            //         itemsRef.current[fieldIntIndex + 1].focus();
+            //     }else{
+            //         itemsRef.current[fieldIntIndex].focus();
+            //     }
+            // })  
             itemsRef.current[fieldIntIndex + 1].focus();
         }
-            // itemsRef.current = []
     }
 
+    
 
     const handleSubmit = (e : React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
         compare(word, CORRECT_WORD)
         dispatch({type:"ADD-WORD", payload: word})
         dispatch({type:"ATTEMPTS", payload: rowIdx})
+    }
+
+    function backSpaceFunc(e:any){
+        const [, codeFieldIndex] = e.target.name.split("-");
+        let fieldIntIndex: any = parseInt(codeFieldIndex, 10);
+        if(e.keyCode === 8 || e.keyCode === 37){
+            if(fieldIntIndex > 0){
+                console.log(fieldIntIndex)
+                itemsRef.current[fieldIntIndex - 1].focus();
+            }
+        }
+        if(e.keyCode === 39){
+            if(fieldIntIndex < COLUMNS-1){
+                itemsRef.current[fieldIntIndex + 1].focus();
+            }
+        }
     }
 
 
@@ -89,6 +124,7 @@ export default function BoardRow({ rowIdx, onScreenInput } : Row) : JSX.Element 
                                 onChange={(e)=>handleInput(e, colIdx)} 
                                 disabled = {attempts === rowIdx || attempts < (rowIdx - 1) ? true : false}
                                 name={`code-${colIdx}`}
+                                onKeyUp={backSpaceFunc}
                             />
                             <button type="submit" style={{display: "none"}}>Submit</button>
                         </>
